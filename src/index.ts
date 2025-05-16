ad7d6e18ff91a1205db4380e8654032343fa9a34
Tinh Pham <pvtinh@tma.com.vn>
2025-05-16 10:27:58 +0700

    Add .env file and connect mongodb

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
 import express from "express";
 import bodyParser from "body-parser";
+import 'dotenv/config'
+import mongoose from "mongoose"
+
 
 const app = express();
 const port = process.env.PORT || 3001;
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
 
 app.use(bodyParser.json());
+
 
 app.get("/", (req, res)=> {
     res.status(200).json({
@@ -13,6 +18,13 @@ app.get("/", (req, res)=> {
     })
 })
 
-app.listen(port, () => {
-    console.log("listening on port " + port);
-})
+mongoose.connect(MONGODB_URI)
+    .then(()=> {
+        console.log("Connected!");
+        app.listen(port, () => {
+            console.log("listening on port " + port);
+        })
+    })
+    .catch((error) => {
+        console.error(error);
+    })
